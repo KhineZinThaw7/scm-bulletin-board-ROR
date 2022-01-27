9147d37b552d974a63d57643d4df0ab768d78d08
KhineZinThaw7 <scm.khinezinthaw@gmail.com>
2022-01-27 15:58:20 +0630

    chart and graph

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,4 +5,16 @@ class DashboardController < ApplicationController
   # dashboard view
   def index
   end
+
+  # user chart
+  def userChart
+    @userCount = User.group("DATE(created_at)").count
+    render template: "dashboard/user-chart"
+  end
+
+  # post chart
+  def postChart
+    @postCount = Post.group("DATE(created_at)").count
+    render template: "dashboard/post-chart"
+  end
 end
