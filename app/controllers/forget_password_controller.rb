581d5d7ed000344ca85372c5b5d2757c1ce1870e
KhineZinThaw7 <scm.khinezinthaw@gmail.com>
2022-01-31 08:16:59 +0630

    add comment

--- app/controllers/forget_password_controller.rb
+++ app/controllers/forget_password_controller.rb
@@ -1,11 +1,13 @@
 class ForgetPasswordController < ApplicationController
   skip_before_action :authorized, only: [:forgetPassword, :forgetPasswordEmail, :new,  :resetPassword]
 
+  # to make forget password
   def forgetPassword
     @categories = Category.all
     render template: "forget_password/send_email"
   end
 
+  # mail send to user forget password
   def forgetPasswordEmail
     @user = User.find_by(email: params[:email]) 
     if @user
@@ -18,11 +20,13 @@ class ForgetPasswordController < ApplicationController
     end
   end
 
+  # reset password view
   def new
     @categories = Category.all
     render template: "forget_password/reset_password"
   end
 
+  # make reset password
   def resetPassword
     @user = User.find_by(email: params[:email])
     if @user && @user.update(password: params[:password])
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   post 'logout', to: 'sessions#destroy'
 
+  # forget password and reset password
   get 'forget-password', to: 'forget_password#forgetPassword'
   post 'forget-password', to: 'forget_password#forgetPasswordEmail'
   get 'reset-password', to: 'forget_password#new'
